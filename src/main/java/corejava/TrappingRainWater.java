2529e73779ef9cc9235e9ba5342e7d479730f8cb
deepakkumar.singh <deepaksinghpw@gmail.com>
2024-11-22 12:26:35 +0530

    22-11-2024/2

--- src/main/java/corejava/TrappingRainWater.java
+++ src/main/java/corejava/TrappingRainWater.java
@@ -3,8 +3,11 @@ package corejava;
 public class TrappingRainWater {
     public static void main(String[] args) {
 
-        int[] height = {0, -1, 0, 3, 0, 1, 0, 4, 0, 2};
+        int[] height = {0, 1, 0, 3, 0, 1, 0, 4, 0, 2};
         System.out.println("Trapped Water: " + trap(height));
+
+        int[] height1 = {3,-2,1,0,2,0,5};
+        System.out.println("Trapped Water for negative : " + trap(height1));
     }
 
     public static int trap(int[] height) {
@@ -13,7 +16,14 @@ public class TrappingRainWater {
         }
 
         int n = height.length;
+        int totalWater = 0;
 
+        for (int i = 1; i < n; i++) {
+            if(height[i]<0){
+                totalWater =-height[i] +totalWater;
+                height[i]=0;
+            }
+        }
 
         int[] leftMax = new int[n];
         int[] rightMax = new int[n];
@@ -31,7 +41,7 @@ public class TrappingRainWater {
         }
 
 
-        int totalWater = 0;
+
         for (int i = 0; i < n; i++) {
             totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
